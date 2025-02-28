8242276980a1c800f6fdcc294466149406b7a2eb
DaanHollands <daan.hollands@student.uhasselt.be>
2025-02-28 12:26:56 +0100

    Added main structure

--- Controller/main.py
+++ Controller/main.py
@@ -3,20 +3,24 @@ from controller import Robot, Motor
 TIME_STEP = 64
 MAX_SPEED = 6.28
 
-# Create the Robot instance.
-robot = Robot()
-leftMotor = robot.getDevice('left wheel motor')
-rightMotor = robot.getDevice('right wheel motor')
+def main():
+    # Create the Robot instance.
+    robot = Robot()
+    leftMotor = robot.getDevice('left wheel motor')
+    rightMotor = robot.getDevice('right wheel motor')
 
-leftMotor.setPosition(float('inf'))
-rightMotor.setPosition(float('inf'))
+    leftMotor.setPosition(float('inf'))
+    rightMotor.setPosition(float('inf'))
 
-leftMotor.setVelocity(0.1 * MAX_SPEED)
-rightMotor.setVelocity(0.1 * MAX_SPEED)
+    leftMotor.setVelocity(0.1 * MAX_SPEED)
+    rightMotor.setVelocity(0.1 * MAX_SPEED)
 
-print("Connected to Webots simulation. Robot name:", robot.getName())
+    print("Connected to Webots simulation. Robot name:", robot.getName())
 
-# Main control loop
-while robot.step(TIME_STEP) != -1:
-    # Add your control logic here.
-    pass
+    # Main control loop
+    while robot.step(TIME_STEP) != -1:
+        # Add your control logic here.
+        pass
+
+if __name__ == '__main__':
+    main()
